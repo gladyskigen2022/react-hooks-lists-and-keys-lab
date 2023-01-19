0ad426c3b754ecefe9ef8c4c3a25a148351e331c
GladysKigen <gladys.kigen@student.moringa.com>
2023-01-19 11:11:07 +0300

    Update

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,11 +3,19 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const ProjectList = projects.map(project => {
+    return (
+      <ProjectItem {...project} key={project.id} />
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+    {ProjectList}
     </div>
+    </div>
+    
   );
 }
 
